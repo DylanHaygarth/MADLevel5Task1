0a7cccb18b9d40ddf1f94eee9ac9bb36b899ce67
dylan2050 <dyhayg@gmail.com>
2020-10-18 12:26:51 +0200

    add note fragment is working

--- app/src/main/java/com/example/madlevel5task1/ui/AddNoteFragment.kt
+++ app/src/main/java/com/example/madlevel5task1/ui/AddNoteFragment.kt
@@ -5,12 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.example.madlevel5task1.R
 import kotlinx.android.synthetic.main.fragment_add_note.*
 
 
 class AddNoteFragment : Fragment() {
+    private val viewModel: NoteViewModel by viewModels()
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -24,7 +28,33 @@ class AddNoteFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         btnSave.setOnClickListener {
-            findNavController().navigate(R.id.notepadFragment)
+            saveNote()
         }
+
+        observeNote()
+    }
+
+    private fun observeNote() {
+        //fill the text fields with the current text and title from the view model
+        viewModel.note.observe(viewLifecycleOwner, Observer {
+                note  ->
+            note?.let {
+                tilTitle.editText?.setText(it.title)
+                tilNote.editText?.setText(it.text)
+            }
+        })
+
+        viewModel.error.observe(viewLifecycleOwner, Observer { message ->
+            Toast.makeText(activity, message, Toast.LENGTH_SHORT).show()
+        })
+
+        viewModel.success.observe(viewLifecycleOwner, Observer { success ->
+            //"pop" the backstack, this means we destroy this fragment and go back to the RemindersFragment
+            findNavController().popBackStack()
+        })
+    }
+
+    private fun saveNote() {
+        viewModel.updateNote(tilTitle.editText?.text.toString(), tilNote.editText?.text.toString())
     }
 }
