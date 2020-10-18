e06655c122fb8725f6b07d75b8f9887ea9196894
dylan2050 <dyhayg@gmail.com>
2020-10-18 12:05:07 +0200

    Connecting notepad fragment to the view model

--- app/src/main/java/com/example/madlevel5task1/ui/NotepadFragment.kt
+++ app/src/main/java/com/example/madlevel5task1/ui/NotepadFragment.kt
@@ -1,13 +1,16 @@
 package com.example.madlevel5task1.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.madlevel5task1.R
+import kotlinx.android.synthetic.main.fragment_notepad.*
 
 class NotepadFragment : Fragment() {
+    private val viewModel: NoteViewModel by viewModels()
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -20,5 +23,17 @@ class NotepadFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        observeAddNoteResult()
+    }
+
+    // observes the live data from the view model. Whenever the live data changes, the notepad is updated
+    private fun observeAddNoteResult() {
+        viewModel.note.observe(viewLifecycleOwner, androidx.lifecycle.Observer { note ->
+            note?.let {
+                tvNoteTitle.text = it.title
+                tvLastUpdated.text = getString(R.string.date, it.date.toString())
+                tvNoteText.text = it.text
+            }
+        })
     }
 }
